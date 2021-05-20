61dc78362bb9e85ad6471b19da244b4f26f0b235
Vishalnannvare <83110881+Vishalnannvare@users.noreply.github.com>
2021-05-20 11:08:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Spark Foundation Bank </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+	<div class="header" id="topheader">
+
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+         <div class="container text-uppercase p-2">
+         	
+         
+ 
+
+  <a class="navbar-brand font-weight-bold text-white" href="#">Spark Foundation Bank</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="createuser.php">Create a User</a>
+        <li class="nav-item">
+        <a class="nav-link" href="transfermoney.php">Make a Transaction</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transactionhistory.php">Transaction Histor</a>
+      </li>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+<section class="header-section">
+	<div class="center-div">
+		<h1 class="font-weight-bold "> Welcome to Spark Foundation Bank </h1>
+		<p class="font-weight-bold ">
+			Best Bank  
+		</p>
+	</div>
+	
+</section>
+</div>
+
+<section class="header-extradiv">
+	<div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="images/user.jpg" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><button style="background-color : #2785C4;">Create a User</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="images/transfer.jpg" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color : #2785C4;">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="images/history.jpg" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color : #2785C4;">Transaction History</button></a>
+                  </div>
+            </div>
+
+
+            <section class="my-5">
+			<div class="py-5">
+				<h2 class="text-center">Thanks For Choosing Us !</h2>
+			</div>
+				</section>
+
+
+	
+</section class="footer">
+
+<footer>
+					<p class="p-3 bg-dark text-white text-center">&copy 2021. Made by <b>VISHAL NANNVARE</b> <br> Spark Bank Foundation</p>
+
+
+ 
+</footer>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
